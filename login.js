1372c96e5d05e3f36f79d9df5bac3bc083cd31f1
priyanshu18012 <priyanshu18012000@gmail.com>
2022-07-23 17:56:05 +0530

    sign up page completesd

---
+++ login.js
@@ -0,0 +1,34 @@
+// function openForm() {
+//     document.getElementById("myForm").style.display = "block";
+//   }
+  
+//   function closeForm() {
+//     document.getElementById("myForm").style.display = "none";
+//   }
+  
+  // ----------------------------------functionality part begin here------------------------------
+  
+  var form = document.querySelector("form");
+    var userData = JSON.parse(localStorage.getItem("userData"));
+  
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+      var data = {
+        email: form.email.value,
+        password: form.password.value,
+      };
+  
+      if (userData === null) {
+        alert("Please Create an Account");
+      } else if (
+        userData.email == data.email &&
+        userData.password == data.password
+      ) {
+        alert("Log In Successful");
+        localStorage.setItem("login", JSON.stringify(userData));
+        window.location.href = "Home.html";
+      } else {
+        alert("user Does Not Exists");
+      }
+    });
+  
